e7020716bc4e097454e10320ee63b6f1c5a13ca0
Seabstián Amado <104456947+sebamoctezuma@users.noreply.github.com>
2022-05-26 00:42:13 -0300

    Resolucion Ej7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            String msg = "";
+            for (int j = 0; j < 7 - i; j++) {
+                msg = msg + "*";
+            }
+            System.out.println(msg);
+        }
 
     }
 }
