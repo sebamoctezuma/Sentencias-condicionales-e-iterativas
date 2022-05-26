0b0fb62365608cf08f5af32cbeba92e33380f3a8
sebamoctezuma <sebamoctezuma@hotmail.com>
2022-05-25 21:20:53 -0300

    Resolucion Ej1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int numeroUno = scanner.nextInt();
+        System.out.println("Ingrese un segundo numero: ");
+        int numeroDos = scanner.nextInt();
+        if (numeroUno < numeroDos) {
+            System.out.println("El numero " + numeroDos + " es mayor entre los ingresados");
 
+        } else if (numeroUno > numeroDos) {
+            System.out.println("El numero " + numeroUno + " es mayor entre los ingresados");
+
+        } else {
+            System.out.println("Ambos numeros son iguales");
+        }
     }
 }
