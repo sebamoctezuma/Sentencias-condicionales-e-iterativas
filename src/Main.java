f06e50fd9c0b2bc12e09ffb9817cd5da427ac1da
Seabstián Amado <104456947+sebamoctezuma@users.noreply.github.com>
2022-05-26 00:49:54 -0300

    Resolucion Ej8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            String msg = "";
+            for (int j = 0; j < 6 - i; j++) {
+                msg = msg + " ";
+            }
+            for (int j = 0; j < i + 1; j++) {
+                msg = msg + "*";
+        }
+            System.out.println(msg);
 
+        }
     }
 }
